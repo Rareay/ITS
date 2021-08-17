0a4b401fb7c7dfc62fb32fed132f548fabe1e16b
tanrui <985480276@qq.com>
2021-08-17 23:14:51 +0800

    初始化工程，无功能代码，可正常编译

---
+++ include/its/fun.h
@@ -0,0 +1,9 @@
+#ifndef ITS_FUN_H
+#define ITS_FUN_H
+
+
+#include <iostream>
+#define NUM 100
+
+
+#endif
---
+++ src/fun.cpp
@@ -0,0 +1,8 @@
+#include "its/fun.h"
+
+int main()
+{
+    std::cout << "hello" << std::endl;
+    std::cout << NUM << std::endl;
+    return 0;
+}
